0ef9ad0e5a2602f64cdf84193b708142717d6b18
조영환 <whdudghks45629@gmail.com>
2021-02-12 11:20:19 +0000

    for_code_test_1~3

---
+++ src/for/2_10950.cpp
@@ -0,0 +1,14 @@
+//A+B - 3
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, c;
+	cin >> a;
+	for (int i = 0; i < a; i++) {
+		cin >> b >> c;
+		cout << b + c << endl;
+	}
+	
+	return 0;
+}
---
+++ src/for/3_8393.cpp
@@ -0,0 +1,15 @@
+//합
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	b = 0;
+	cin >> a;
+	for (int i = 1; i <= a; i++) {
+		b += i;
+	}
+	cout << b << endl;
+	
+	return 0;
+}
